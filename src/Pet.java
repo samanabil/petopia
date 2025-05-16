e8e27ba776ced66696fcecdf2ac044aee859494e
samanabil <160977243+samanabil@users.noreply.github.com>
2025-05-16 23:13:18 +0300

    Delete src directory

--- src/Pet.java
+++
@@ -1,7 +0,0 @@
-// Pet and Appointment
-class Pet {
-    //every object of the pet class has a name, age, and a type
-    String name;
-    int age;
-    String type;
-}
--- src/Veteran.java
+++
@@ -1,5 +0,0 @@
-class Veteran extends Profile {
-    //veterans has the option to manage schedules or view appointments
-    public void manageSchedule() {System.out.println("Managing schedules");}
-    public void viewAppointments() {System.out.println("Viewing appointments");}
-}
